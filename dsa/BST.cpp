74262ddddfbe36910d19405b03ae2519a9bd8dc4
dihnhuunam <dihnhuunam@gmail.com>
2023-05-14 00:21:37 +0700

    insert search delete bst

---
+++ dsa/BST.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+}node;
+
+//tạo node 
+node *makeNode(int data)
+{
+    // node *newNode = (node*)malloc(sizeof(node));
+    node *newNode = new node();
+    newNode -> data = data;
+    newNode -> left = NULL;
+    newNode -> right = NULL;
+    return newNode;
+}
+
+//chèn node
+node* Insert(node *&root, int data)
+{
+    if(root == NULL)
+    {
+        root = makeNode(data);
+    }
+    else if(data <= root -> data)
+    {
+        root -> left = Insert(root -> left, data);
+    }
+    else
+    {
+        root -> right = Insert(root -> right, data);
+    }
+    return root;
+}
+
+//tìm node
+bool Search(node *root, int data)
+{
+    if(root == NULL) return false;
+    else if(root -> data == data) return true;
+    else if(data <= root -> data) return Search(root -> left, data);
+    else return Search(root -> right, data);
+}
+
+node *FindMin(node *root)
+{
+    while(root -> left != NULL)
+    {
+        root = root -> left;
+    }
+    return root;
+}
+
+node *FindMax(node *root)
+{
+    while(root -> right = NULL)
+    {
+        root = root -> right;
+    }
+    return root;
+}
+
+node *Delete(node *&root, int data)
+{
+    if(root == NULL) return root;
+    else if(data < root -> data) root -> left = Delete(root -> left, data);
+    else if(data > root -> data) root -> right = Delete(root -> right, data);
+    else if(data == root -> data)
+    {
+        //case1: no child
+        if(root -> left == NULL && root -> right == NULL)
+        {
+            delete root;
+            root = NULL;
+        }
+        //case2: 1 child
+        else if(root -> left == NULL)
+        {
+            node *temp = root;
+            root = root -> right;
+            delete temp;
+        }
+        else if(root -> right == NULL)
+        {
+            node *temp = root;
+            root = root -> left;
+            delete temp;
+        }
+        //case3: 2 children
+        else 
+        {
+            node *temp = FindMin(root -> right);
+            root -> data = temp -> data;
+            root -> right = Delete(root -> right, temp -> data);
+        }
+    }
+    return root;
+}
+int main()
+{
+
+    return 0;
+}
